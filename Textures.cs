2fff9ef93d4fad2f7428d2dc518d3ce06a3bf72d
USKamicadze <uskamicadze@gmail.com>
2015-08-18 17:42:50 +1000

    add spritesheets

--- Textures.cs
+++ Textures.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+
+using TexturePackerLoader;
 namespace Match3
 {
     static class Textures
@@ -14,10 +16,15 @@ namespace Match3
 
         static public Texture2D playButton;
         static public Texture2D okButton;
-        
+
+        static public SpriteSheet glowingBall;
+        static public SpriteSheet bombBonus;
+        static public SpriteSheet explosion;
         static public SpriteFont font;
 
+        static public Texture2D test;
         static public Dictionary<Element.Type, Texture2D> elements = new Dictionary<Element.Type, Texture2D>();
+
         static public void Load(Microsoft.Xna.Framework.Content.ContentManager Content)
         {
             background = Content.Load<Texture2D>("Images/background");
@@ -31,7 +38,10 @@ namespace Match3
             elements[Element.Type.Red] = Content.Load<Texture2D>("Images/red");
             elements[Element.Type.Yellow] = Content.Load<Texture2D>("Images/yellow");
             font = Content.Load<SpriteFont>("font");
+            var spriteSheetLoader = new SpriteSheetLoader(Content);
+            glowingBall = spriteSheetLoader.Load("Images/glowingBall");
+            explosion = spriteSheetLoader.Load("Images/Explosion");
+            bombBonus = spriteSheetLoader.Load("Images/bombBonus");
         }
     }
-
 }
