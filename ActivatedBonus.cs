38eb5efaae65940403fb0ea7aaa3d4b773b3d830
USKamicadze <uskamicadze@gmail.com>
2015-08-18 18:31:53 +1000

    refactor

---
+++ ActivatedBonus.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using TexturePackerLoader;
+
+namespace Match3
+{
+    class ActivatedBonusBase : DrawableGameComponent
+    {
+        public float scale = 1;
+        public bool ended = false;
+        public ActivatedBonusBase(Game game) : base(game) { }
+
+    }
+
+    class LineBonus : ActivatedBonusBase
+    {
+        float shift;
+        Vector2 start;
+        Vector2 position;
+        Vector2 end;
+        float duration = 1;
+        float elapsed = 0;
+        AnimatedTexture texture;
+
+        public delegate void LineBonusEventHandler(LineBonus lineBonus, LineBonusEventAgrs e);
+        public event LineBonusEventHandler onEnd;
+        public event LineBonusEventHandler onUpdate;
+
+        public class LineBonusEventAgrs : EventArgs
+        {
+            public Vector2 prevPosition;
+            public Vector2 newPosition;
+            public LineBonusEventAgrs(Vector2 prevPosition, Vector2 newPosition)
+            {
+                this.prevPosition = prevPosition;
+                this.newPosition = newPosition;
+            }
+        }
+        public LineBonus(Game game, Vector2 start, Vector2 end, Vector2 dir, float shift)
+            : base(game)
+        {
+            this.texture = new GlowingBallTexture(game.GraphicsDevice, 16, 16);
+            this.shift = shift;
+            this.position = start;
+            this.end = end + dir * Config.tile.Size.ToVector2() / 2;
+            this.start = start;
+            Vector2 d = (this.start - this.end) / shift;
+            this.duration = Math.Abs(d.X - d.Y);
+            this.position = start;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            float elapsedGameTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (!ended) {
+                elapsed += elapsedGameTime;
+                Vector2 prevPos = position;
+                position = start + (end - start) * elapsed / duration;
+                texture.Update(gameTime);
+                if (onUpdate != null)
+                    onUpdate(this, new LineBonusEventAgrs(prevPos, position));
+                if (duration < elapsed) {
+                    ended = true;
+                    position = end;
+                    if (onEnd != null)
+                        onEnd(this, new LineBonusEventAgrs(prevPos, position));
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (!ended) {
+                SpriteBatch sb = Game.Services.GetService<SpriteBatch>();
+                SpriteRender sr = Game.Services.GetService<SpriteRender>();
+                sb.Begin();
+                texture.Draw(gameTime, sr, position, scale);
+                sb.End();
+                base.Draw(gameTime);
+            }
+        }
+
+    }
+
+    class BombBonus : ActivatedBonusBase
+    {
+
+
+        public Element el { get; private set; }
+        AnimatedTexture explosionTexture;
+        double elapsed = 0;
+        bool exploded = false;
+        public delegate void BombBonusEventHandler(BombBonus bombBonus);
+        public event BombBonusEventHandler onExplosion;
+        public BombBonus(Game game, Element el)
+            : base(game)
+        {
+            this.explosionTexture = new ExplosionTexture(GraphicsDevice, Config.tile.Width * 3, Config.tile.Height * 3);
+            this.el = el;
+            this.scale = 5;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            elapsed += gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (elapsed < Config.bombBonusDuration) {
+                float v = 255 - (float)(255 * elapsed / Config.bombBonusDuration);
+                el.color = new Color(255, v, v);
+            } else if (!exploded) {
+                exploded = true;
+                el.color = Color.Red;
+                if (onExplosion != null)
+                    onExplosion(this);
+            }
+            if (exploded)
+                explosionTexture.Update(gameTime);
+            if (explosionTexture.ended) ended = true;
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (!ended) {
+                if (!exploded) {
+                    if (el.dying)
+                        el.Draw(gameTime);
+                } else {
+                    SpriteBatch sb = Game.Services.GetService<SpriteBatch>();
+                    SpriteRender sr = Game.Services.GetService<SpriteRender>();
+                    sb.Begin();
+                    explosionTexture.Draw(gameTime, sr, el.rectangle.Center.ToVector2(), scale);
+                    sb.End();
+                }
+            }
+            base.Draw(gameTime);
+        }
+    }
+
+}
--- Field.cs
+++ Field.cs
@@ -64,137 +64,6 @@ namespace Match3
                         
         }
 
-        class ActivatedBonusBase : DrawableGameComponent
-        {
-            public float scale = 1;
-            public bool ended = false;
-            public ActivatedBonusBase(Game game) : base(game) {}
-            
-        }
-
-        class LineBonus : ActivatedBonusBase
-        {
-            float shift;
-            Vector2 start;
-            Vector2 position;
-            Vector2 end;
-            float duration = 1;
-            float elapsed = 0;
-            AnimatedTexture texture;
-
-            public delegate void LineBonusEventHandler(LineBonus lineBonus, LineBonusEventAgrs e);
-            public event LineBonusEventHandler onEnd;
-            public event LineBonusEventHandler onUpdate;
-            
-            public class LineBonusEventAgrs : EventArgs
-            {
-                public Vector2 prevPosition;
-                public Vector2 newPosition;
-                public LineBonusEventAgrs(Vector2 prevPosition, Vector2 newPosition)
-                {
-                    this.prevPosition = prevPosition;
-                    this.newPosition = newPosition;
-                }
-            }
-            public LineBonus(Game game, Vector2 start, Vector2 end, Vector2 dir, float shift) : base (game)
-            {
-                this.texture =  new GlowingBallTexture(game.GraphicsDevice, 16, 16);
-                this.shift = shift;
-                this.position = start;
-                this.end = end + dir * Config.tile.Size.ToVector2() / 2;
-                this.start = start;
-                Vector2 d = (this.start - this.end) / shift;
-                this.duration = Math.Abs(d.X - d.Y);
-                this.position = start;
-            }
-
-            public override void Update(GameTime gameTime)
-            {
-                float elapsedGameTime = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
-                if (!ended) {
-                    elapsed += elapsedGameTime;
-                    Vector2 prevPos = position;
-                    position = start + (end - start) * elapsed / duration;
-                    texture.Update(gameTime);
-                    if (onUpdate != null)
-                        onUpdate(this, new LineBonusEventAgrs(prevPos, position));
-                    if (duration < elapsed) {
-                        ended = true;
-                        position = end;
-                        if (onEnd != null)
-                            onEnd(this, new LineBonusEventAgrs(prevPos, position));
-                    }
-                }
-                base.Update(gameTime);
-            }
-
-            public override void Draw(GameTime gameTime)
-            {
-                if (!ended) {
-                    SpriteBatch sb = Game.Services.GetService<SpriteBatch>();
-                    SpriteRender sr = Game.Services.GetService<SpriteRender>();
-                    sb.Begin();
-                    texture.Draw(gameTime, sr, position, scale);
-                    sb.End();
-                    base.Draw(gameTime);
-                }
-            }
-            
-        }
-
-        class BombBonus : ActivatedBonusBase        {
-
-            
-            public Element el {get; private set;}
-            AnimatedTexture explosionTexture;
-            double elapsed = 0;
-            bool exploded = false;
-            public delegate void BombBonusEventHandler(BombBonus bombBonus);
-            public event BombBonusEventHandler onExplosion;
-            public BombBonus(Game game, Element el)
-                : base(game)
-            {
-                this.explosionTexture = new ExplosionTexture(GraphicsDevice, Config.tile.Width * 3, Config.tile.Height * 3);
-                this.el = el;
-                this.scale = 5;
-            }
-
-            public override void Update(GameTime gameTime)
-            {
-                elapsed += gameTime.ElapsedGameTime.TotalMilliseconds;
-                if (elapsed < Config.bombBonusDuration) {
-                    float v = 255 - (float)(255 * elapsed / Config.bombBonusDuration);
-                    el.color = new Color(255, v, v);
-                } else if (!exploded) {
-                    exploded = true;
-                    el.color = Color.Red;
-                    if (onExplosion != null)
-                        onExplosion(this);
-                }
-                if (exploded)
-                    explosionTexture.Update(gameTime);
-                if (explosionTexture.ended) ended = true;
-                base.Update(gameTime);
-            }
-
-            public override void Draw(GameTime gameTime)
-            {
-                if (!ended) {
-                    if (!exploded) {
-                        if (el.dying)
-                            el.Draw(gameTime);
-                    } else {
-                        SpriteBatch sb = Game.Services.GetService<SpriteBatch>();
-                        SpriteRender sr = Game.Services.GetService<SpriteRender>();
-                        sb.Begin();
-                        explosionTexture.Draw(gameTime, sr, el.rectangle.Center.ToVector2(), scale);
-                        sb.End();
-                    }
-                }
-                base.Draw(gameTime);
-            }
-
-        }
 
         private List<ActivatedBonusBase> activatedBonuses = new List<ActivatedBonusBase>(); 
 
@@ -221,10 +90,6 @@ namespace Match3
             return el;
         }
 
-        Element CloneElement(Element el)
-        {
-            return CreateElement(el.col, el.row, el.type, el.rectangle);
-        }
         void Init()
         {
             field = new Element[size, size];
@@ -471,6 +336,20 @@ namespace Match3
             }
             return matchedElements;
         }
+
+        //helper function
+        private bool isNewBonusElement(Element el, List<Element>[,] matchedElements)
+        {
+            List<Element> curMatched = matchedElements[el.row, el.col];
+            return curMatched.Count > 3 && (
+                curMatched.TrueForAll(e => matchedElements[e.row, e.col].Count < curMatched.Count)
+                || (
+                    GetLastMoved(curMatched) == el
+                &&  
+                    curMatched.TrueForAll(e => matchedElements[e.row, e.col].Count <= curMatched.Count)
+                )
+            );
+        }
         private void FindMatchesAndDestroy()
         {
             List<Element>[,] matchedElements = FindAllMatches();
@@ -479,16 +358,7 @@ namespace Match3
                     Element el = field[i, j];
                     List<Element> curMatched = matchedElements[i,j];
                     if (el == EMPTY || curMatched.Count < 3) continue;
-                    if (curMatched.Count > 3 &&
-                       (
-                                curMatched.TrueForAll(e => 
-                                    matchedElements[e.row, e.col].Count < curMatched.Count)
-                            || (
-                                    GetLastMoved(curMatched) == el 
-                                && curMatched.TrueForAll(e =>
-                                    matchedElements[e.row, e.col].Count <= curMatched.Count))
-                       )
-                    ){
+                    if (isNewBonusElement(el, matchedElements)){
                         ActivateBonus(el);
                         if (curMatched.Count == 4){
                             Element el2 = curMatched.Find(e => e != el);
@@ -502,7 +372,6 @@ namespace Match3
                     }
                 }
             }
-            
         }
 
         private bool MatchExists()
@@ -522,7 +391,10 @@ namespace Match3
         private int CalcLine(Point start, Point shift, Element.Type type)
         {
             int count = 0;
-            for (Point cur = start; 0 <= cur.X && cur.X < size && 0 <= cur.Y && cur.Y < size && GetElementByPoint(cur).type == type; cur += shift)
+            for (Point cur = start; 
+                !OutOfRange(cur) && GetElementByPoint(cur).type == type; 
+                cur += shift
+            )
                 ++count;
             return count;
         }
@@ -531,12 +403,10 @@ namespace Match3
         {
             List<Element> line = new List<Element>();
             for (Point cur = start; 
-                    !OutOfRange(cur)
-                    && GetElementByPoint(cur).type == type; 
-                cur += shift) 
-            {
+                !OutOfRange(cur) && GetElementByPoint(cur).type == type; 
+                cur += shift
+            ) 
                 line.Add(GetElementByPoint(cur));
-            }
             return line;
         }
 
@@ -667,9 +537,8 @@ namespace Match3
 
         private void UpdateEvents(GameTime gameTime)
         {
-            for (int i = 0; i < events.Count; ++i) {
+            for (int i = 0; i < events.Count; ++i)
                 events[i].Invoke();
-            }
             events.Clear();
         }
 
@@ -685,21 +554,18 @@ namespace Match3
                 FindMatchesAndDestroy();
                 MakeElementFall();
             }
-            if (events.Count == 0) {
+            if (events.Count == 0)
                 UpdateElements(gameTime);
-            }
             UpdateBonuses(gameTime);
             UpdateEvents(gameTime);
         }
 
         private void DrawElements(GameTime gameTime)
         {
-            for (int i = 0; i < size; ++i) {
-                for (int j = 0; j < size; ++j) {
+            for (int i = 0; i < size; ++i)
+                for (int j = 0; j < size; ++j)
                     if (field[i, j] != EMPTY)
                         field[i, j].Draw(gameTime);
-                }
-            }
         }
 
         public override void Draw(GameTime gameTime)
